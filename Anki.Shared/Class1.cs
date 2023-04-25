4ccb3db451d8fb8e4d67371544ea288e125c4913
Leandro Arruda <leandrolevvi@hotmail.com>
2023-04-25 20:33:01 -0300

    Iniciando estrutura do projeto

---
+++ Anki.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Anki.Shared;
+public class Class1
+{
+
+}
---
+++ Anki.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Anki.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
