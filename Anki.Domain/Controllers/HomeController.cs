fcfc841940eeeb7da0586644d3fdf9ab4fa786d1
Leandro Arruda <leandrolevvi@hotmail.com>
2023-04-25 23:07:58 -0300

    Adicionado controllers

---
+++ Anki.Domain/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anki.Domain.Controllers
+{
+    [ApiController]
+    public class HomeController: ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return Ok("Ok");
+        }
+
+        [HttpGet("/v1")]
+        public IActionResult GetV1()
+        {
+            return Ok("/v1 ativa.");
+        }
+    }
+}
--- Anki.Domain/Program.cs
+++ Anki.Domain/Program.cs
@@ -1,6 +1,14 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
