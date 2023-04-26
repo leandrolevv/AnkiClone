f04c5b443697cd133cce346fca4214c40957d898
Leandro Arruda <leandrolevvi@hotmail.com>
2023-04-25 23:01:01 -0300

    Criando entidades

---
+++ Anki.Domain/Entities/Card.cs
@@ -0,0 +1,11 @@
+﻿using Anki.Shared.Entities;
+
+namespace Anki.Domain.Entities
+{
+    public class Card : Entity
+    {
+        public string Front { get; set; }
+        public string Back { get; set; }
+        public IList<Tag> Tags { get; set; } = new List<Tag>();
+    }
+}
---
+++ Anki.Domain/Entities/Tag.cs
@@ -0,0 +1,11 @@
+﻿using Anki.Shared.Entities;
+
+namespace Anki.Domain.Entities
+{
+    public class Tag : Entity
+    {
+        public string Text { get; set; }
+
+        public IList<Card> Cards { get; set; }
+    }
+}
