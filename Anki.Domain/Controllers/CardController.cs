5ae4216b4994d1b86c58618482b2f018a4342c81
Leandro Arruda <leandrolevvi@hotmail.com>
2023-04-25 23:35:08 -0300

    Adicionado controlador do Card -> Get e Post

---
+++ Anki.Domain/Controllers/CardController.cs
@@ -0,0 +1,35 @@
+﻿using Anki.Domain.DbContexts;
+using Anki.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anki.Domain.Controllers
+{
+    [ApiController]
+    public class CardController: ControllerBase
+    {
+        [HttpGet("/v1/Card")]
+        public async Task<IActionResult> GetAsync([FromServices] AnkiDbContext context)
+        {
+            var cards = await context.Cards.AsNoTracking().ToListAsync();
+
+            return Ok(cards);
+        }
+
+        [HttpPost("/v1/Card")]
+        public async Task<IActionResult> PostAsync([FromServices] AnkiDbContext context, [FromBody] Card model)
+        {
+            var card = new Card()
+            {
+                Front = model.Front,
+                Back = model.Back,
+                Tags = model.Tags
+            };
+
+            await context.Cards.AddAsync(card);
+            await context.SaveChangesAsync();
+            
+            return Ok(card);
+        }
+    }
+}
--- Anki.Domain/Program.cs
+++ Anki.Domain/Program.cs
@@ -1,14 +1,15 @@
 using System.Text.Json.Serialization;
+using Anki.Domain.DbContexts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
 });
+builder.Services.AddDbContext<AnkiDbContext>();
 
 var app = builder.Build();
-
 app.MapControllers();
 
 app.Run();
